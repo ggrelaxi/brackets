62e56518bc3d63ff9cf7e5c288d9c1b28ddde4bb
Alex Smirnov <ggrelaxi@gmail.com>
2023-01-26 01:45:08 +0300

    feat: finish brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const opened = [];
+    const closed = [];
+
+    bracketsConfig.forEach((item) => {
+        const [open, close] = item;
+        opened.push(open);
+        closed.push(close);
+    });
+
+    if (str.length <= 1) return true;
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        let currentBracket = str[i];
+        if (opened.includes(currentBracket) && !closed.includes(currentBracket)) {
+            stack.push(currentBracket);
+        } else if (opened.includes(currentBracket) && closed.includes(currentBracket)) {
+            if (stack[stack.length - 1] !== currentBracket) {
+                stack.push(currentBracket);
+            } else {
+                stack.pop();
+            }
+        } else {
+            let current = stack.pop();
+            if (!current) return false;
+
+            if (opened.indexOf(current) !== closed.indexOf(currentBracket)) return false;
+        }
+    }
+    return stack.length === 0;
+};
